cd6cb7d6897a94a0090159a01fab9970ea6e2b72
aravind <koyyalaaravind@gmail.com>
2022-11-02 20:26:39 -0500

    boxing

--- routes/Boxing.js
+++ routes/Boxing.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('engines', { title: 'Search Results Engines' });
+  res.render('Boxing', { title: 'Search Results Boxing' });
 });
 
 module.exports = router;
